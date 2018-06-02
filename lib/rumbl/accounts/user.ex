3bbbdb28f545f4c7eb2d3c3d2f22230c4764f4ae
Leandro Marques <leandrolmarques@gmail.com>
2018-06-02 19:31:47 -0300

    Add password validation and hashing

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,10 +1,12 @@
 defmodule Rumbl.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Rumbl.Accounts.Credential
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
