e21726b059b3560f623cc37ff32a02b5f99228f8
Leandro Marques <leandrolmarques@gmail.com>
2018-06-03 10:59:40 -0300

    Preload user when loading videos

--- lib/rumbl/multimidia/multimidia.ex
+++ lib/rumbl/multimidia/multimidia.ex
@@ -19,7 +19,9 @@ defmodule Rumbl.Multimidia do
 
   """
   def list_videos do
-    Repo.all(Video)
+    Video
+    |> Repo.all()
+    |> preload_user()
   end
 
   @doc """
@@ -36,7 +38,7 @@ defmodule Rumbl.Multimidia do
       ** (Ecto.NoResultsError)
 
   """
-  def get_video!(id), do: Repo.get!(Video, id)
+  def get_video!(id), do: preload_user(Repo.get!(Video, id))
 
   @doc """
   Creates a video.
@@ -114,15 +116,21 @@ defmodule Rumbl.Multimidia do
     Video
     |> user_videos_query(user)
     |> Repo.all()
+    |> preload_user()
   end
 
   def get_user_video!(%Accounts.User{} = user, id) do
     from(v in Video, where: v.id == ^id)
     |> user_videos_query(user)
     |> Repo.one!()
+    |> preload_user()
   end
 
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
     from(v in query, where: v.user_id == ^user_id)
+  end
+
+  defp preload_user(video_or_videos) do
+    Repo.preload(video_or_videos, :user)
   end
 end
